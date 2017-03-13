e32c762e2e4b74ac8ef6db762c2fd24013eaca88
killerHELIX <rharris4@mail.umw.edu>
2017-03-13 16:37:34 -0400

    added external stylesheet

---
+++ styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	bigTitle: {
+		textAlign: 'center',
+		fontSize: 25,
+		fontWeight: 'bold',
+		paddingBottom: 10,
+	},
+
+	title: {
+
+		textAlign: 'left',
+		fontSize:20, 
+		fontWeight: 'bold',
+	},
+
+	info: {
+		textAlign: 'left',
+		fontSize: 15,
+		fontWeight: 'normal',
+	},
+});
+
+module.exports = styles
+
+
