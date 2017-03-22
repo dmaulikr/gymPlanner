b95b848230c58ead0a4dfab407cb4c6fe3aafa2e
killerHELIX <rharris4@mail.umw.edu>
2017-03-22 17:45:19 -0400

    added input styles

--- styles.js
+++ styles.js
@@ -22,6 +22,27 @@ const styles = StyleSheet.create({
 		fontWeight: 'normal',
 		paddingLeft: 20,
 	},
+
+	input: {
+		textAlign: 'left',
+		fontSize: 15,
+		fontWeight: 'normal',
+		marginLeft: 20,
+		marginBottom: 10,
+		paddingLeft: 5,
+		height: 25,
+		width: 200,
+		borderWidth: 1,
+	},
+
+	inputButton: {
+		marginLeft: 20,
+		marginBottom: 10,
+		paddingLeft: 5,
+		height: 25,
+		width: 100,
+		borderWidth: 1,
+	},
 });
 
 module.exports = styles
